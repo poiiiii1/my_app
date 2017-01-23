4f93777a6ca9a0dbbc593064998663b11fb5d30a
yhm <poiiiii136@gmail.com>
2017-01-23 19:53:14 +0900

    add server stylesheets

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
-app.get('/',function(req,res) {
-  res.send('Hello World');
-});
-
- app.listen(3000,function(){
-   console.log('Server On!');
+app.use(express.static(path.join(__dirname + '/public')));
+console.log(__dirname);
+app.listen(3000,function(){
+   console.log('Server On!!!!');
  });
